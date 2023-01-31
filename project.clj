dc0732ba206d10a4e26360472c9fc05a19f1bb13
Ackerley Tng <ackerleytng@gmail.com>
2023-01-31 21:58:54 +0800

    Update dependencies

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :uberjar-name "logaze.jar"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ring "1.9.5"]
+                 [ring "1.9.6"]
                  [ring-cors "0.1.13"]
                  [enlive "1.1.6"]
                  [clj-http "3.12.3"]
                  [cheshire "5.11.0"]
-                 [org.clojure/core.async "1.5.648"]
+                 [org.clojure/core.async "1.6.673"]
                  [slingshot "0.12.2"]]
   :ring {:handler logaze.core/handler}
   :plugins [[lein-ring "0.12.6"]]
